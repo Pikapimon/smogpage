5746c3471e512a6ab6dcb714f60c5fcf19163035
pikapimon <pikapimon@icloud.com>
2019-12-09 15:03:40 +0800

    modi

--- prediction/urls.py
+++ prediction/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'prediction'
 urlpatterns = [
     path('', views.homepage, name="home"),
+    path('ajax-province-refresh/', views.history_refresh,
+         name="history-province-refresh"),
+
 ]
--- prediction/views.py
+++ prediction/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import JsonResponse
 # Create your views here.
+import json
 
 
 def homepage(request):
     return render(request, 'prediction/index.html', {})
+
+
+def history_refresh(request):
+    a = {1: 2}
+    return JsonResponse(a)
